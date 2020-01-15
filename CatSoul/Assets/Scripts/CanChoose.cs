6849d0acdca65ab95578dfb827ada0971de1cdec
570189458 <570189458@qq.com>
2020-01-15 17:17:41 +0800

    part prefabs

---
+++ CatSoul/Assets/Scripts/CanChoose.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanChoose : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("OnMouseEnter");
+        this.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("OnMouseExit");
+        this.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
---
+++ CatSoul/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public float GBScale = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "GB")
+        {
+            this.GetComponent<Rigidbody2D>().gravityScale = GBScale;
+        }
+        else if(collision.tag == "GF")
+        {
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.Lerp(this.GetComponent<Rigidbody2D>().velocity, Vector2.zero, 0.8f);
+            this.GetComponent<Rigidbody2D>().gravityScale = -this.GetComponent<Rigidbody2D>().gravityScale;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "GB")
+        {
+            this.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+        }
+    }
+
+}
